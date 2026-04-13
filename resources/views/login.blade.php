9d7893d741b9680520fc6002dbfa2e4fc9ed8ed9
vegaschoolskommda <vegafadanputraschool@gmail.com>
2026-04-13 09:56:16 +0700

    pertama commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login-Karyawan</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+    <header>
+        <nav>
+                <div class="logo">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo Perusahaan">
+                </div>
+                <ul class="nav-links">
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                    <li><a href="{{ url('/about') }}">About Us</a></li>
+                    <li><a href="{{ url('/contact') }}">Contact</a></li>
+                </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="login">
+            <div class="login-form">
+                
+            </div>
+        </section>
+    </main>
+</body>
+</html>
